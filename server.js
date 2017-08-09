1ba1fea47cd3be9112d2a12df0511033efaa19ad
kalyanainala <kalyanainala55@gmail.com>
2017-08-09 12:41:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',  function (req, res) {
-    res.sendFile(path.join("Article one requested and will be served here"));
+    res.sendFile(path.join('Article one requested and will be served here'));
 });
 
 app.get('/article-two',  function (req, res) {
