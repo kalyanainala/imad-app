642206c904d2b3542aaa15b4bb891f0b6fb68ce9
kalyanainala <kalyanainala55@gmail.com>
2017-08-10 12:11:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-<html>
-    <head>
-        <title>
-            Article one I am kalyan
-        </title>
-        </head>
-        <body>
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr/>
-            <h3>
-                Article One
-            </h3>
-            <div>
-                Aug 9th, 2017
-            </div>
-            <div>
-                <p>
-                    Content for my first article
-                </p>
-                <p>
-                    Content for my first article
-                </p>
-                <p>
-                    Content for my first article
-                </p>
-                
-            </div>
-            
-        </body>
-        
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-</html>
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one',  function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/article-two',  function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three',  function (req, res) {
+    res.send(path.join("Article three requested and will be served here"));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
