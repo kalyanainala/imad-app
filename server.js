be6531e1ef84b72dcc4964462203f02170eaedc1
kalyanainala <kalyanainala55@gmail.com>
2017-08-24 10:46:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-    user: 'coco98',
-    database: 'coco98',
+    user: 'kalyanainala5536',
+    database: 'kalyanainala5536',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
