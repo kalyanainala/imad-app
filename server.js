24b14d54128a8a29d53c24c674755a536ffc524a
kalyanainala <kalyanainala55@gmail.com>
2017-08-24 10:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     user: 'kalyanainala5536',
     database: 'kalyanainala5536',
     host: 'db.imad.hasura-app.io',
-    port: '8080',
+    port: '5432',
     password: process.env.DB_PASSWORD
 };
 
